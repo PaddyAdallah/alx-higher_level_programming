b86bb8eeb38d04a30e84ea2953148786bcb28656
PaddyAdallah <adalapaddy9@gmail.com>
2022-09-02 18:13:09 +0000

    More data structures -sets , dictionaries

--- 0x04-python-more_data_structure/0-square_matrix_simple.py
+++ 0x04-python-more_data_structure/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-	sq_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
-	for i in range(len(matrix)):
-		for j in range(len(matrix[i])):
-			sq_matrix[i][j] = matrix[i][j] ** 2
-	return sq_matrix
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
